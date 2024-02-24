f7a29d9089a198e58d265723a1f06abfd035de01
soniamartinezz <sonia93mp@gmail.com>
2024-02-24 22:35:37 +0100

    index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-const { dbConnection } = require('./config/db');
-
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-dbConnection();
-
-app.listen(PORT, () => {
-    console.log(`Servidor en http://localhost:${PORT}`)
-});
+sonia
