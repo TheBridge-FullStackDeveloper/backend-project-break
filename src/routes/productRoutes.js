4c0deb889220aaf4d35dde407594a6d64631062a
JAVI <javitxin1977@gmail.com>
2024-02-29 18:31:16 +0100

    working4

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {ProductController} = require('../controllers/productController.js');
+const { ProductController } = require('./controllers/productController.js');
 
 router.get('/', ProductController.showProducts);
 router.get('/products', ProductController.showProducts);
@@ -10,16 +10,16 @@ router.get('/products/category/:category', ProductController.showProductCategory
 
 // dashboard
 router.get('/login', ProductController.login)
-router.get('/dashboard',  ProductController.showProducts);
+router.get('/dashboard', ProductController.showProducts);
 router.get('/dashboard/new', ProductController.showNewProduct);
 
 router.post('/dashboard', ProductController.createProduct);
-router.get('/dashboard/:productId',  ProductController.showProductById);
+router.get('/dashboard/:productId', ProductController.showProductById);
 
-router.get('/dashboard/:productId/edit',  ProductController.showEditProduct);
+router.get('/dashboard/:productId/edit', ProductController.showEditProduct);
 router.post('/dashboard/:productId', ProductController.updateProduct);
 
-router.get('/dashboard/:productId/delete',  ProductController.deleteProduct);
+router.get('/dashboard/:productId/delete', ProductController.deleteProduct);
 router.get('/logout', ProductController.logout)
 
 
